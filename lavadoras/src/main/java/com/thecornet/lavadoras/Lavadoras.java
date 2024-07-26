bdd74d50fa909ea8ddb7a985452314253054822c
Jordi Sanchis Hernandez <juan_@ldmachine.com>
2024-07-26 14:25:42 +0200

    he hecho el primer cambio

--- lavadoras/src/main/java/com/thecornet/lavadoras/Lavadoras.java
+++ lavadoras/src/main/java/com/thecornet/lavadoras/Lavadoras.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.thecornet.lavadoras;
 
-/**
- *
- * @author juan_
- */
+
 public class Lavadoras {
 
     public static void main(String[] args) {
